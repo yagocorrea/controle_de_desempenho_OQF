a3f025998d4dac46d2b21a1dc40fa26b0ba1c87f
Rielson Leandro/DTISS/UN01/Moura <rielson.leandro@grupomoura.com>
2021-06-19 17:50:08 -0300

    Inicio do projeto

---
+++ controledesempenhooqf/src/main/java/br/com/oqf/controledesempenhooqf/ControledesempenhooqfApplication.java
@@ -0,0 +1,13 @@
+package br.com.oqf.controledesempenhooqf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledesempenhooqfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledesempenhooqfApplication.class, args);
+	}
+
+}
---
+++ controledesempenhooqf/src/test/java/br/com/oqf/controledesempenhooqf/ControledesempenhooqfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oqf.controledesempenhooqf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledesempenhooqfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
